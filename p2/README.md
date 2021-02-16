95bdb221ec32927af85c4d2578e431e15befe969
Keryn Egan <keryn.egan@gmail.com>
2021-02-16 17:35:14 -0500

    Created local hosts for each project ahead of time

---
+++ p2/README.md
@@ -0,0 +1,2 @@
+# eX
+Dummy for eventual P2 project
---
+++ p2/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <title>Hello from p2</title>
+    <meta charset='utf-8'>
+</head>
+<body>
+    <h1>Hello from p2</h1>
+</body>
+</html>
