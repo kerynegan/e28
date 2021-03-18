81c0424ddd327f81ddeabaa483510fa93a070635
Keryn Egan <keryn.egan@gmail.com>
2021-03-17 23:42:24 -0400

    p1 updates (ignore previous commits; messy files were irrelevant

--- p1/README.md
+++ p1/README.md
@@ -3,7 +3,9 @@
 + Production URL: <http://e28p1.yourdomain.com>
 
 ## Outside resources
-TBD
++ [This StackOverflow question](https://stackoverflow.com/questions/44681646/the-page-will-strangely-refresh-when-i-click-the-button) which helped me figure out why my page kept reloading on submit of my buttons. 
++ And [this related documentation from Mozilla](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button) which further explained the functionality.
++ 
 
 ## Notes for instructor
 TBD
--- p1/script.js
+++ p1/script.js
@@ -1,10 +1,29 @@
 const Game = {
     data() {
         return {
-            item: '',
-            items: []
-        }
+            playerName: '',
+            minimumValue: 25,
+            maximumValue: 100,
+            randomNumber: 55,
+            currentNumber: 54,
+            playerSelection: '',
+            computerSelection: '',
+            turn: 'Player',
+            choices: [],
+            gameStarted: false,
+            gameOver: false
+            
+        }   
     },
+    methods: {
+        beginGame() {
+            this.gameStarted = true;
+        },
+        makeSelection() {
+            this.choices.push(Number(this.playerSelection));
+        }
+        
+    }
 
 }
 
