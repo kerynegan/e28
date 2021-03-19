847f38692cfbc60abca850892beb3741acb1a4bd
Keryn Egan <keryn.egan@gmail.com>
2021-03-18 21:20:15 -0400

    Functional and styling updates

--- p1/README.md
+++ p1/README.md
@@ -5,7 +5,11 @@
 ## Outside resources
 + [This StackOverflow question](https://stackoverflow.com/questions/44681646/the-page-will-strangely-refresh-when-i-click-the-button) which helped me figure out why my page kept reloading on submit of my buttons. 
 + And [this related documentation from Mozilla](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button) which further explained the functionality.
-+ 
++ [This page](https://vuejs.org/v2/guide/forms.html) on v-model binding modifiers.
++ I re-reviewed the v-if/v-if-else conditionals on [this page](https://vuejs.org/v2/guide/conditional.html).
++ I heavily used [this tooltips styling page from W3Schools](https://www.w3schools.com/css/css_tooltip.asp) for the hover effects on the game tallies.
++ I didn't realize we could easily [use strings for list-style-items](https://developer.mozilla.org/en-US/docs/Web/CSS/list-style-type) until this project. 
++ Added some Bootstrap CSS styling and referenced [that documentation](https://getbootstrap.com/docs/3.4/css/).
 
 ## Notes for instructor
 TBD
--- p1/script.js
+++ p1/script.js
@@ -4,23 +4,101 @@ const Game = {
             playerName: '',
             minimumValue: 25,
             maximumValue: 100,
-            randomNumber: 55,
-            currentNumber: 54,
-            playerSelection: '',
-            computerSelection: '',
-            turn: 'Player',
+            randomNumber: '',
+            currentNumber: '',
+            selection: '',
+            turn: 'player',
             choices: [],
             gameStarted: false,
-            gameOver: false
+            gameOver: false,
+            gamemessage: '',
+            errorAlert: '',
+            message: "Let's begin! Choose a number:",
+            myInterval:'',
             
         }   
     },
     methods: {
+        checkName() {
+            if(this.playerName === '') {
+                this.errorAlert = "You must enter a name.";
+            } 
+            
+        },
         beginGame() {
+            this.turn = this.playerName;
+            this.errorAlert = '';
             this.gameStarted = true;
+            this.getStartingNumber();
+        },
+        getStartingNumber() {
+            this.maximumValue = Math.ceil(this.maximumValue);
+            this.minimumValue = Math.floor(this.minimumValue);
+            if (this.maximumValue < this.minimumValue) {
+                var x = this.maximumValue;
+                this.maximumValue = this.minimumValue
+                this.minimumValue = x;
+            };
+            var random = Math.random();
+            var w = this.maximumValue - this.minimumValue + 1;
+            this.randomNumber = Math.floor(random * w + this.minimumValue);
+            if (this.randomNumber <= 1) {
+                this.message = "Oops, the random number is too low to play. Let's start again."
+                this.gameOver = true;
+            }
+            this.currentNumber = this.randomNumber;
         },
         makeSelection() {
-            this.choices.push(Number(this.playerSelection));
+            if(this.selection != 0) {
+                this.errorAlert = '';
+                clearInterval(this.myInterval);
+                var obj = {
+                    who: this.turn, 
+                    what: this.selection
+                };
+                this.choices.push(obj);
+                this.checkCount();
+            } else {
+                this.errorAlert = 'You must choose a number first.';
+            }
+        },
+        checkCount() {
+            this.currentNumber -= this.selection;
+            this.selection = '';
+            if (this.currentNumber <= 1) {
+                if (this.turn == "player") {
+                    this.message = "The computer is stuck.";
+                    this.alertGame("Winner");
+                } else if (this.turn == "The computer") {
+                    this.message = "Ugh, you're stuck!";
+                    this.alertGame("Loser");
+                }
+            this.gameOver = true;
+            } else if (this.currentNumber > 1) {
+                this.changeTurn();
+            }
+        },
+        changeTurn() {
+            if (this.turn == this.playerName) {
+                this.message = "The computer is thinking..."
+                this.turn = "The computer";
+                this.myInterval = setInterval(function () { app.computerTurn(); }, 1500);
+            } else {
+                this.message = "Your turn! Play a number:"
+                this.turn = this.playerName;
+            }
+        },
+        computerTurn() {
+            var rand = Math.random();
+            if(this.currentNumber > 3) {
+                this.selection = Math.floor(rand * 3 + 1);
+            } else if(this.currentNumber <= 3){
+                this.selection = Math.floor(rand * 2 + 1);
+            }
+            this.makeSelection();
+        },
+        alertGame(x) {
+
         }
         
     }
