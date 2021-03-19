a6537ac2ced4aa790b93bdae9079acc49c9aaed4
Keryn Egan <keryn.egan@gmail.com>
2021-03-19 13:12:21 -0400

    Function and style updates

--- p1/README.md
+++ p1/README.md
@@ -3,15 +3,18 @@
 + Production URL: <http://e28p1.yourdomain.com>
 
 ## Outside resources
+### Functionality 
 + [This StackOverflow question](https://stackoverflow.com/questions/44681646/the-page-will-strangely-refresh-when-i-click-the-button) which helped me figure out why my page kept reloading on submit of my buttons. 
 + And [this related documentation from Mozilla](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button) which further explained the functionality.
 + [This page](https://vuejs.org/v2/guide/forms.html) on v-model binding modifiers.
 + I re-reviewed the v-if/v-if-else conditionals on [this page](https://vuejs.org/v2/guide/conditional.html).
-+ ### Styling
+### Styling
 + *UPDATE:* I removed this after adding Bootstrap styling, but leaving the reference here for completeness: I heavily used [this tooltips styling page from W3Schools](https://www.w3schools.com/css/css_tooltip.asp) for the hover effects on the game tallies.
 + *UPDATE:* I didn't use this after all: I didn't realize we could easily [use strings for list-style-items](https://developer.mozilla.org/en-US/docs/Web/CSS/list-style-type) until this project. 
 + Added some Bootstrap CSS styling and referenced [that documentation](https://getbootstrap.com/docs/3.4/css/).
-+ Styling guide for making radio selections look like buttons [from Mark Heath](https://markheath.net/post/customize-radio-button-css)
++ Styling guide for making radio selections look like buttons [from Mark Heath](https://markheath.net/post/customize-radio-button-css). (I was inspired by the demo-ed Heads or Tails game that used pictures for radio buttons in class)
++ [Google fonts](https://fonts.googleapis.com/css2?family=Lora:wght@700&family=Merriweather&display=swap) Lora and Merriweather
++ [Favicon generator](https://favicon.io/favicon-generator/) saved me from the rabbit hole I fall into every time I open Photoshop and got rid of that annoying little '404 ico not found' warning.
 
 ## Notes for instructor
 TBD
--- p1/script.js
+++ p1/script.js
@@ -2,8 +2,8 @@ const Game = {
     data() {
         return {
             playerName: '',
-            minimumValue: 25,
-            maximumValue: 100,
+            minimumValue: 15,
+            maximumValue: 25,
             randomNumber: '',
             currentNumber: '',
             selection: '',
@@ -19,18 +19,26 @@ const Game = {
         }   
     },
     methods: {
+        //alert if name field is left empty
         checkName() {
             if(this.playerName === '') {
                 this.errorAlert = "You must enter a name.";
             } 
-            
         },
+        //sets the first turn to the player's name, clears any error messages, and starts the game
         beginGame() {
             this.turn = this.playerName;
             this.errorAlert = '';
             this.gameStarted = true;
             this.getStartingNumber();
         },
+        /*
+        - Gets the starting randomNumber from the range provided by the player
+        - Assigns that value to the currentNumber to start. 
+        - If the numbers are the same, it just sets the randomNumber to the value. 
+        - If the player submits a max that is lower than the min, this switches them.
+        - The game then waits for the player to make a choice.
+        */
         getStartingNumber() {
             this.maximumValue = Math.ceil(this.maximumValue);
             this.minimumValue = Math.floor(this.minimumValue);
@@ -39,15 +47,25 @@ const Game = {
                 this.maximumValue = this.minimumValue
                 this.minimumValue = x;
             };
-            var random = Math.random();
-            var w = this.maximumValue - this.minimumValue + 1;
-            this.randomNumber = Math.floor(random * w + this.minimumValue);
-            if (this.randomNumber <= 1) {
-                this.message = "Oops, the random number is too low to play. Let's start again."
-                this.gameOver = true;
+            if (this.maximumValue === this.minimumValue) {
+                this.randomNumber = this.maximumValue
+            } else {
+                var random = Math.random();
+                var w = this.maximumValue - this.minimumValue + 1;
+                this.randomNumber = Math.floor(random * w + this.minimumValue);
+                if (this.randomNumber <= 1) {
+                    this.message = "Oops, the random number is too low to play. Let's start again."
+                    this.gameOver = true;
+                }
             }
             this.currentNumber = this.randomNumber;
         },
+        /* 
+        When either the player or the computer makes a move, 
+        - an object is pushed to the choices array for the game tally (with selection + who played it)
+        - the interval is cleared (so that the computer only makes one guess at a time)
+        - if the player forgets to choose a number before hitting submit, it calls an errorAlert message.
+        */
         makeSelection() {
             if(this.selection != 0) {
                 this.errorAlert = '';
@@ -62,6 +80,7 @@ const Game = {
                 this.errorAlert = 'You must choose a number first.';
             }
         },
+        //Checks whether the game is over. If yes, calls alert. If not, changes turn.
         checkCount() {
             this.currentNumber -= this.selection;
             this.selection = '';
@@ -78,6 +97,7 @@ const Game = {
                 this.changeTurn();
             }
         },
+        //Changes the turn and updates a message to player.
         changeTurn() {
             if (this.turn == this.playerName) {
                 this.message = "The computer is thinking..."
@@ -88,6 +108,12 @@ const Game = {
                 this.turn = this.playerName;
             }
         },
+        /*
+        The computer randomly plays a number. 
+        - If the current number is > 3, it plays 1-3; 
+        - If the current number is < 3, it plays 1 or 2. 
+        (remember, game ends if current number <= 1)
+        */
         computerTurn() {
             var rand = Math.random();
             if(this.currentNumber > 3) {
